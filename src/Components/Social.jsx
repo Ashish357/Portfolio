d79b31f3554689e4ac71cf1ae054ad9368fcdf4f
Ashish Saha <49359207+Ashish357@users.noreply.github.com>
2024-06-02 20:18:06 +0530

    Update Social.jsx

--- src/Components/Social.jsx
+++ src/Components/Social.jsx
@@ -10,17 +10,17 @@ const Social = () => {
         {/* Hamburger Icon */}
         <ul className={`${showLinks ? 'sm:block' : 'hidden'} sm:flex flex-col gap-2`}>
             <li className='border-2 cursor-pointer border-red-600 rounded-full hover:bg-red-600'>
-                <a href="https://github.com/Ashish357" target='_blank' rel="noopener noreferrer">
+                <a href="https://github.com/Ashish357" target='_blank' rel="noopener noreferrer" aria-label="Ashish Saha's GitHub">
                     <BsGithub className='text-red-600 text-2xl sm:text-3xl rounded-full hover:text-white'/>
                 </a>
             </li>
             <li className='border-2 cursor-pointer border-red-600 rounded-full hover:bg-red-600'>
-                <a href="https://www.linkedin.com/in/ashish-saha-b09608184" target='_blank' rel="noopener noreferrer">
+                <a href="https://www.linkedin.com/in/ashish-saha-b09608184" target='_blank' rel="noopener noreferrer" aria-label="Ashish Saha's LinkedIn">
                     <BsLinkedin className='text-red-600 text-2xl sm:text-3xl rounded-full hover:text-white'/>
                 </a>
             </li>
             <li className='border-2 cursor-pointer border-red-600 rounded-full hover:bg-red-600'>
-                <a href="mailto:ashishsaha2708@gmail.com" target='_blank' rel="noopener noreferrer">
+                <a href="mailto:ashishsaha2708@gmail.com" target='_blank' rel="noopener noreferrer" aria-label="Email Ashish Saha">
                     <ImMail4 className='text-red-600 text-2xl sm:text-3xl rounded-full hover:text-white'/>
                 </a>
             </li>
