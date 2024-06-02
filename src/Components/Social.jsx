7d83acc1f5c735d339ed153b66998f312d1fd754
ashish357 <ashishsaha357@gmail.com>
2024-06-02 22:33:41 +0530

    Fixed social Links

--- src/Components/Social.jsx
+++ src/Components/Social.jsx
@@ -8,7 +8,7 @@ const Social = () => {
   return (
     <div className='fixed z-10 bottom-4 left-4'>
         {/* Hamburger Icon */}
-        <ul className={`${showLinks ? 'sm:block' : 'sm:hidden'} flex flex-col gap-2`}>
+        <ul className={`${showLinks ? 'visible' : 'max-sm:hidden'} flex flex-col gap-2`}>
             <li className='border-2 cursor-pointer border-red-600 rounded-full hover:bg-red-600'>
                 <a href="https://github.com/Ashish357" target='_blank' rel="noopener noreferrer" aria-label="Ashish Saha's GitHub">
                     <BsGithub className='text-red-600 text-2xl sm:text-3xl rounded-full hover:text-white'/>
